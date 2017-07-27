79e563eb27d98ce84701f0ed86597a8258c52ea4
gaoxiaodiao <805888196@qq.com>
2017-07-27 17:36:33 +0800

    测试:获取post请求数据

--- Test.py
+++ Test.py
@@ -7,11 +7,18 @@
 *开发环境:Kali Linux/Python v2.7.13
 """
 from modules import cdn_check
+import requests
+#测试从url中获取域名
 def test_get_domain():
     print cdn_check.get_domain("http://blog.csdn.net/imzoer/article/details/8636764")
     print cdn_check.get_domain("http://www.cnblogs.com/allenblogs/archive/2011/11/15/2055149.html")
     print cdn_check.get_domain("https://my.oschina.net/guol/blog/95699")
     print cdn_check.get_domain("")
-
+#测试表单数据
+def test_get_post_request_info():
+    url = "http://ce.cloud.360.cn"
+    web_content = requests.get(url)
+    print cdn_check.get_post_request_info(web_content.text)
 if __name__ == '__main__':
-    test_get_domain()
+    #test_get_domain()
+    test_get_post_request_info()
