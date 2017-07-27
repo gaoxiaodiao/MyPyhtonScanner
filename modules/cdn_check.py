d015844edc0147ef9da91219f12ef2081cb12fb8
gaoxiaodiao <805888196@qq.com>
2017-07-27 17:36:13 +0800

    检测cdn:获取post请求数据

--- modules/cdn_check.py
+++ modules/cdn_check.py
@@ -5,7 +5,8 @@
 *作者:高小调
 *创建时间:2017年07月27日 星期四 15时39分27秒
 *开发环境:Kali Linux/Python v2.7.13
-*参考于某个大神的github:https://github.com/Xyntax/POC-T/blob/2.0/script/cdn-detect.py
+*
+*本代码参考于:https://github.com/Xyntax/POC-T/blob/2.0/script/cdn-detect.py
 *结果(未使用):
 *http://forexample.com Nodes:XX IP(1):XXX:XXX:XXX:XXX
 *结果(使用):
@@ -30,3 +31,13 @@ def get_domain(url):
     #(sheme='http',netloc='forexample',path='/index.asp',params='',query='id=xxx&cate=xxx',fargment='')
     tmp = urlparse.urlparse(url)                                    #解析url
     return urlparse.urlunsplit([tmp.scheme,tmp.netloc,'','',''])    #拼接url
+
+#获取http://ce.cloud.360.cn/表单的信息,目的是构造Post请求参数
+def get_post_request_info(web_content):
+    _ret_dict = {}
+    soup = BeautifulSoup(web_content,"html.parser")
+    for each_input_elem in soup.find_all('input'):
+        if 'name' in each_input_elem.attrs and 'value' in each_input_elem.attrs: 
+            #print each_input_elem['name'] + ":"+ each_input_elem['value']
+            _ret_dict[each_input_elem['name']] = each_input_elem['value']
+    return _ret_dict
