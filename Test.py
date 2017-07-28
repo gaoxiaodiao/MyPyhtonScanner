c62fa6b802036ff40ad55ca9e9d4683e5a873f2e
gaoxiaodiao <805888196@qq.com>
2017-07-28 15:15:29 +0800

    测试端口扫描

--- Test.py
+++ Test.py
@@ -7,22 +7,38 @@
 *开发环境:Kali Linux/Python v2.7.13
 """
 from modules import cdn_check
+from modules import port_scan
 import requests
+from sys import argv
 #测试从url中获取域名
 def test_get_domain():
     print cdn_check.get_domain("http://blog.csdn.net/imzoer/article/details/8636764")
     print cdn_check.get_domain("http://www.cnblogs.com/allenblogs/archive/2011/11/15/2055149.html")
     print cdn_check.get_domain("https://my.oschina.net/guol/blog/95699")
     print cdn_check.get_domain("")
+
 #测试表单数据
 def test_get_post_request_info():
     url = "http://ce.cloud.360.cn"
     web_content = requests.get(url)
     print cdn_check.get_post_request_info(web_content.text)
+
 #测试cdn检测
 def test_cdn_check():
-    print cdn_check.run("http://gaoxiaodiao.com")
+    script,first = argv
+    msg,state=cdn_check.run(first)
+    print msg
+
+#测试端口扫描
+def test_port_scan():
+    script,ip = argv
+    if ip == None:
+        print "Usage:%s [ip]"%script
+        return False
+    ps = port_scan.PortScan(ip)
+    ps.run()
 if __name__ == '__main__':
     #test_get_domain()
     #test_get_post_request_info()
-    test_cdn_check()
+    #test_cdn_check()
+    test_port_scan()
